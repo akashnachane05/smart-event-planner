32d4401a290058d785da90faa033d3fb1d44eb79
akashnachane05 <nachaneakash@gmail.com>
2025-06-11 04:42:00 +0530

    for deployment

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path = require('path');
 dotenv.config();
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 app.use(express.json());
 
 // Routes
